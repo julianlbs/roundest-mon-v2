49061926d83b4f8684f67aa652839a65acbeff0b
julianlbs <julianlimalbs@gmail.com>
2022-09-28 11:16:21 -0300

    Add random pokemon id fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,19 @@
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import { trpc } from "@/utils/trpc";
+import { useState } from "react";
 
 export default function Home() {
-	const { data, isLoading } = trpc.hello.useQuery({ text: "client" });
+	const [ids, setIds] = useState(getOptionsForVote());
+	const [first, second] = ids;
 
-	if (isLoading) return <div>Loading...</div>;
-	if (data) return <div>{data.greeting}</div>;
 	return (
 		<div className="h-screen w-screen flex flex-col justify-center items-center">
 			<div className="text-2xl">Whick Pokémon is rounder?</div>
 			<div className="p-2" />
 			<div className="p-8 flex flex-row justify-between items-center max-w-2xl border rounded">
-				<div className="w-16 h-16 bg-red-600">First</div>
+				<div className="w-16 h-16 bg-red-600">{first}</div>
 				<div className="p-2">Vs</div>
-				<div className="w-16 h-16 bg-red-600">Second</div>
+				<div className="w-16 h-16 bg-red-600">{second}</div>
 			</div>
 		</div>
 	);
