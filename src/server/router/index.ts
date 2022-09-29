44011fa606547cb932a80ffa77d9495fb203370e
julianlbs <julianlimalbs@gmail.com>
2022-09-29 09:52:03 -0300

    Setup prisma and connect to db

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,22 +1,27 @@
 import { initTRPC } from '@trpc/server';
 import { PokemonClient } from 'pokenode-ts';
 import { z } from 'zod';
-
+import { prisma } from '@/server/utils/prisma';
 export const t = initTRPC.create();
 
 export const appRouter = t.router({
   getPokemonById: t.procedure
-    .input(
-      z
-        .object({
-          id: z.number()
-        }),
-    )
+    .input(z.object({ id: z.number() }))
     .query(async ({ input }) => {
       const api = new PokemonClient();
       const pokemon = await api.getPokemonById(input.id);
       return { name: pokemon.name, sprites: pokemon.sprites };
     }),
+  castVote: t.procedure
+    .input(z.object({ votedFor: z.number(), votedAgainst: z.number() }))
+    .mutation(async ({ input }) => {
+      const voteInDb = await prisma.vote.create({
+        data: {
+          ...input
+        }
+      });
+      return { success: true, vote: voteInDb };
+    })
 });
 
 // export type definition of API
