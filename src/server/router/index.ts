92ac786a9d03a98696e8270713118f675cebbf1f
julianlbs <julianlimalbs@gmail.com>
2022-09-29 13:14:13 -0300

    Change api connection name

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -8,8 +8,8 @@ export const appRouter = t.router({
   getPokemonById: t.procedure
     .input(z.object({ id: z.number() }))
     .query(async ({ input }) => {
-      const api = new PokemonClient();
-      const pokemon = await api.getPokemonById(input.id);
+      const pokeApiConnection = new PokemonClient();
+      const pokemon = await pokeApiConnection.getPokemonById(input.id);
       return { name: pokemon.name, sprites: pokemon.sprites };
     }),
   castVote: t.procedure
