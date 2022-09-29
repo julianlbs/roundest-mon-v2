da5c5d189e9ce1fbd590e78cbce816a697486a7d
julianlbs <julianlimalbs@gmail.com>
2022-09-29 14:56:57 -0300

    Script to fetch and fill db with pokemons

---
+++ scripts/fill-db.ts
@@ -0,0 +1,20 @@
+import { PokemonClient } from 'pokenode-ts';
+import { prisma } from "../src/server/utils/prisma";
+
+const doBackFill = async () => {
+  const pokeApiConnection = new PokemonClient();
+
+  const allPokemon = await pokeApiConnection.listPokemons(0, 493);
+  const formattedPokemon = allPokemon.results.map((p, index) =>
+  ({
+    id: index + 1,
+    name: (p as { name: string; }).name,
+    spriteUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+  }));
+
+  const creation = await prisma.pokemon.createMany({ data: formattedPokemon });
+
+  console.log("Creation?", creation);
+};
+
+doBackFill();
